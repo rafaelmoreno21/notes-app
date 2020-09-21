9cfcc08e16c8daf66d89c8cb57fcd8fdf5e40ddd
Rafael Moreno <60928243+rafaelmoreno21@users.noreply.github.com>
2020-09-20 20:33:27 -0400

    Delete password

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://rafaelmoreno21:21092010Rafael@cluster0.ka9et.mongodb.net/notes-app?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://rafaelmoreno21:<password>@cluster0.ka9et.mongodb.net/notes-app?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
